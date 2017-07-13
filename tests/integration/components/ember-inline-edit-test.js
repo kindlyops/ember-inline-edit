64cd39f8a104952ed6fbf9c781c07a011e9ed3bf
Swastik <swastik@aceteq.co>
2017-07-13 18:10:20 +0530

    Fix tests and get rid of jQuery

--- tests/integration/components/ember-inline-edit-test.js
+++ tests/integration/components/ember-inline-edit-test.js
@@ -1,347 +1,354 @@
-import Ember from 'ember';
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from "ember-qunit"
+import hbs from "htmlbars-inline-precompile"
 
-// const down  = $.Event('keyup', { keyCode: 40, which: 40})
-// const up    = $.Event('keyup', { keyCode: 38, which: 38})
-// const enter = $.Event('keyup', { keyCode: 13, which: 13})
-// const esc   = $.Event('keyup', { keyCode: 27, which: 27})
+import { fillIn, click, find, keyEvent } from "ember-native-dom-helpers"
 
-const { $, run } = Ember
+moduleForComponent(
+  "ember-inline-edit",
+  "Integration | Component | ember inline edit",
+  {
+    integration: true,
 
-moduleForComponent('ember-inline-edit', 'Integration | Component | ember inline edit', {
-  integration: true,
+    beforeEach: function() {
+      this.on("onSave", val => {
+        this.set("value", val)
+      })
 
-  beforeEach: function () {
-    this.on('onSave', (val) => {
-      this.set('value', val)
-    })
+      this.on("onClose", () => {
+        this.set("value", "closed")
+      })
 
-    this.on('onClose', () => {
-      this.set('value', 'closed')
-    })
+      this.on("onCancel", () => {
+        this.set("value", "canceled")
+      })
 
-    this.on('onCancel', () => {
-      this.set('value', 'canceled')
-    })
-
-    this.set('value', null)
+      this.set("value", null)
+    }
   }
-})
+)
 
-test('it renders', function (assert) {
+const classNames = {
+  container: ".ember-inline-edit",
+  input: ".ember-inline-edit-input",
+  saveBtn: ".ember-inline-edit-save",
+  cancelBtn: ".ember-inline-edit-cancel",
+  hint: ".ember-inline-edit .hint"
+}
+
+test("it renders", function(assert) {
   this.render(hbs`{{ember-inline-edit
                         value=value
                         onSave="onSave"
                         onClose="onClose"
-                        onCancel="onCancel"}}`);
+                        onCancel="onCancel"}}`)
 
-  assert.equal(this.$('.ember-inline-edit').length, 1)
+  assert.ok(find(classNames.container))
 })
 
-test('the label is default', function (assert) {
+test("the label is default", function(assert) {
   this.render(hbs`{{ember-inline-edit
                         value=value
                         onSave="onSave"
                         onClose="onClose"
-                        onCancel="onCancel"}}`);
+                        onCancel="onCancel"}}`)
 
-  assert.equal(this.$('.ember-inline-edit').text().trim(), 'Not Provided')
+  assert.equal(find(classNames.container).innerText.trim(), "Not Provided")
 })
 
-test('on click, it shows the input and the buttons by default', function (assert) {
+test("on click, it shows the input and buttons", async function(assert) {
   this.render(hbs`{{ember-inline-edit
                         value=value
                         onSave="onSave"
                         onClose="onClose"
-                        onCancel="onCancel"}}`);
+                        onCancel="onCancel"}}`)
 
-  assert.equal(this.$('.ember-inline-edit-input').length, 0)
-  assert.equal(this.$('.ember-inline-edit-save').length, 0)
-  assert.equal(this.$('.ember-inline-edit-cancel').length, 0)
+  assert.notOk(find(classNames.input))
+  assert.notOk(find(classNames.saveBtn))
+  assert.notOk(find(classNames.cancelBtn))
 
-  this.$('.ember-inline-edit').click();
+  await click(classNames.container)
 
-  assert.equal(this.$('.ember-inline-edit-input').length, 1)
-  assert.equal(this.$('.ember-inline-edit-save').length, 1)
-  assert.equal(this.$('.ember-inline-edit-cancel').length, 1)
+  assert.ok(find(classNames.input))
+  assert.ok(find(classNames.saveBtn))
+  assert.ok(find(classNames.cancelBtn))
 })
 
-test('on click, the input gets focus', function (assert) {
+test("on click, the input gets focus", async function(assert) {
   this.render(hbs`{{ember-inline-edit
                         value=value
                         onSave="onSave"
                         onClose="onClose"
-                        onCancel="onCancel"}}`);
+                        onCancel="onCancel"}}`)
 
-  this.$('.ember-inline-edit').click();
-  assert.ok(document.activeElement.classList.contains('ember-inline-edit-input'))
+  await click(classNames.container)
+
+  assert.ok(
+    document.activeElement.classList.contains("ember-inline-edit-input")
+  )
 })
 
-test('it does not render the save button', function (assert) {
+test("it does not render the save button", async function(assert) {
   this.render(hbs`{{ember-inline-edit
                         value=value
                         showSaveButton=false
                         onSave="onSave"
                         onClose="onClose"
-                        onCancel="onCancel"}}`);
+                        onCancel="onCancel"}}`)
 
-  this.$('.ember-inline-edit').click();
-
-  assert.equal(this.$('.ember-inline-edit-save').length, 0)
+  await click(classNames.container)
+  assert.notOk(find(classNames.saveBtn))
 })
 
-test('it does not render the cancel button', function (assert) {
+test("it does not render the cancel button", async function(assert) {
   this.render(hbs`{{ember-inline-edit
                         value=value
                         showCancelButton=false
                         onSave="onSave"
                         onClose="onClose"
-                        onCancel="onCancel"}}`);
+                        onCancel="onCancel"}}`)
 
-  this.$('.ember-inline-edit').click();
-
-  assert.equal(this.$('.ember-inline-edit-cancel').length, 0)
+  await click(classNames.container)
+  assert.notOk(find(classNames.cancelBtn))
 })
 
-test('it renders a non-default save button label', function (assert) {
+test("it renders a non-default save button label", async function(assert) {
   this.render(hbs`{{ember-inline-edit
                         value=value
                         saveLabel="✓"
                         onSave="onSave"
                         onClose="onClose"
-                        onCancel="onCancel"}}`);
+                        onCancel="onCancel"}}`)
 
-  this.$('.ember-inline-edit').click();
-
-  assert.equal(this.$('.ember-inline-edit-save').text().trim(), "✓")
+  await click(classNames.container)
+  assert.equal(find(classNames.saveBtn).innerText.trim(), "✓")
 })
 
-test('it renders a non-default cancel button label', function (assert) {
+test("it renders a non-default cancel button label", async function(assert) {
   this.render(hbs`{{ember-inline-edit
                         value=value
                         cancelLabel="x"
                         onSave="onSave"
                         onClose="onClose"
-                        onCancel="onCancel"}}`);
+                        onCancel="onCancel"}}`)
 
-  this.$('.ember-inline-edit').click();
-
-  assert.equal(this.$('.ember-inline-edit-cancel').text().trim(), "x")
+  await click(classNames.container)
+  assert.equal(find(classNames.cancelBtn).innerText.trim(), "x")
 })
 
-test('on click, it renders the hint if present', function (assert) {
+test("on click, it renders the hint if present", async function(assert) {
   this.render(hbs`{{ember-inline-edit
                         value=value
                         hintLabel="press Enter to save"
                         onSave="onSave"
                         onClose="onClose"
-                        onCancel="onCancel"}}`);
+                        onCancel="onCancel"}}`)
 
-  assert.equal(this.$('.ember-inline-edit .hint').length, 0)
+  assert.notOk(find(classNames.hint))
+  await click(classNames.container)
 
-  this.$('.ember-inline-edit').click();
-
-  assert.equal(this.$('.ember-inline-edit .hint').text().trim(), 'press Enter to save')
+  assert.ok(find(classNames.hint))
+  assert.ok(find(classNames.hint).innerText.trim(), "press Enter to save")
 })
 
-test('on save, it sends the save action', function (assert) {
+test("on save, it sends the save action", async function(assert) {
   this.render(hbs`{{ember-inline-edit
                         value=value
                         onSave="onSave"
                         onClose="onClose"
-                        onCancel="onCancel"}}`);
+                        onCancel="onCancel"}}`)
 
-  this.$('.ember-inline-edit').click()
+  await click(classNames.container)
+  await fillIn(classNames.input, "Something")
+  await click(classNames.saveBtn)
 
-  this.$('.ember-inline-edit-input').val('Something')
-  this.$('.ember-inline-edit-input').trigger('input')
-
-  this.$('.ember-inline-edit-save').click()
-
-  assert.equal(this.get('value'), 'Something')
+  assert.equal(this.get("value"), "Something")
 })
 
-test('on pressing enter in text field, it sends the save action', function (assert) {
+test("on pressing enter in text field, it sends the save action", async function(
+  assert
+) {
   this.render(hbs`{{ember-inline-edit
-                        value=value
+                        value=(readonly value)
                         onSave="onSave"
                         onClose="onClose"
-                        onCancel="onCancel"}}`);
+                        onCancel="onCancel"}}`)
 
-  this.$('.ember-inline-edit').click()
+  await click(classNames.container)
+  await fillIn(classNames.input, "Something Else")
+  await keyEvent(classNames.input, "keyup", 13)
 
-  this.$('.ember-inline-edit-input').val('Something')
-  this.$('.ember-inline-edit-input').trigger('input')
-  this.$('.ember-inline-edit-input').trigger('enter')
-
-  assert.equal(this.get('value'), 'Something')
+  assert.equal(this.get("value"), "Something Else")
+  assert.notOk(find(classNames.input))
 })
 
-test('on pressing enter in textarea field, it does not send the save action', function (assert) {
+test("on pressing enter in textarea field, it does not send the save action", async function(
+  assert
+) {
   this.render(hbs`{{ember-inline-edit
                     value=value
                     field="textarea"
                     onSave="onSave"
                     onClose="onClose"
-                    onCancel="onCancel"}}`);
+                    onCancel="onCancel"}}`)
 
-  this.$('.ember-inline-edit').click()
+  await click(classNames.container)
+  await fillIn(classNames.input, "Something")
+  await keyEvent(classNames.input, "keyup", 13)
 
-  run(() => {
-    this.$('.ember-inline-edit-input').val('Something')
-    this.$('.ember-inline-edit-input').trigger('input')
-    this.$('.ember-inline-edit-input').trigger('enter')
-  })
-
-  assert.equal(this.$('.ember-inline-edit-input').length, 1)
+  assert.ok(find(classNames.input))
 })
 
-test('on cancel, it sends the cancel action and restores the input field to initial state', function (assert) {
+test("on cancel, it sends the cancel action and restores the input field to initial state", async function(
+  assert
+) {
   this.render(hbs`{{ember-inline-edit
                         value=value
                         onSave="onSave"
                         onClose="onClose"
-                        onCancel="onCancel"}}`);
+                        onCancel="onCancel"}}`)
 
-  this.$('.ember-inline-edit').click()
+  await click(classNames.container)
+  await fillIn(classNames.input, "Some initial value")
+  await click(classNames.saveBtn)
 
-  this.$('.ember-inline-edit-input').val('Some initial value')
-  this.$('.ember-inline-edit-input').trigger('input')
-  this.$('.ember-inline-edit-save').click()
+  await click(classNames.container)
+  await fillIn(classNames.input, "Some new value")
+  await click(classNames.cancelBtn)
 
-  this.$('.ember-inline-edit').click()
-
-  this.$('.ember-inline-edit-input').val('Some new value')
-  this.$('.ember-inline-edit-input').trigger('input')
-  this.$('.ember-inline-edit-cancel').click()
-
-  assert.equal(this.get('value'), 'Some initial value')
-  assert.equal(this.$('.ember-inline-edit').text().trim(), 'Some initial value')
+  assert.equal(this.get("value"), "Some initial value")
 })
 
-test('on pressing esc, it sends the close action', function (assert) {
+test("on pressing esc, it sends the close action", async function(assert) {
   this.render(hbs`{{ember-inline-edit
                         value=value
                         onSave="onSave"
                         onClose="onClose"
-                        onCancel="onCancel"}}`);
+                        onCancel="onCancel"}}`)
 
-  assert.equal(this.$('.ember-inline-edit-input').length, 0)
+  assert.notOk(find(classNames.input))
 
-  this.$('.ember-inline-edit').click()
-  assert.equal(this.$('.ember-inline-edit-input').length, 1)
+  await click(classNames.container)
+  assert.ok(find(classNames.input))
 
-  this.$('.ember-inline-edit-input').trigger('esc')
-  assert.equal(this.get('value'), null)
+  this.$(".ember-inline-edit-input").trigger("esc")
+  await keyEvent(classNames.input, "keyup", 27)
+
+  assert.notOk(find(classNames.input))
 })
 
-test('the text field is the same width as the original element', function(assert){
+test("the text field is the same width as the original element", async function(
+  assert
+) {
   this.render(hbs`{{ember-inline-edit
                       value='A long field value, probably at least a few hundred pixels'
                       onSave="onSave"
                       onClose="onClose"
-                      onCancel="onCancel"}}`);
+                      onCancel="onCancel"}}`)
 
-  assert.equal(this.$('.ember-inline-edit-input').length, 0)
+  assert.notOk(find(classNames.input))
 
-  let { width } = document
-    .querySelector('.ember-inline-edit')
-    .getBoundingClientRect()
+  let { width } = find(classNames.container).getBoundingClientRect()
+  await click(classNames.container)
 
-  this.$('.ember-inline-edit').click()
-
-  let inputWidth = document
-    .querySelector('.ember-inline-edit-input')
-    .style
-    .width
-
+  let inputWidth = find(classNames.input).style.width
   assert.equal(inputWidth, `${width + 2}px`)
 })
 
-test('on click, it does nothing if not enabled', function (assert) {
+test("on click, it does nothing if not enabled", async function(assert) {
   this.render(hbs`{{ember-inline-edit
-                    enabled=false}}`);
+                    enabled=false}}`)
 
-  assert.equal(this.$('.ember-inline-edit-input').length, 0)
-  assert.equal(this.$('.ember-inline-edit-save').length, 0)
-
-  this.$('.ember-inline-edit').click();
-  assert.equal(this.$('.ember-inline-edit-input').length, 0)
-  assert.equal(this.$('.ember-inline-edit-save').length, 0)
+  assert.notOk(find(classNames.input))
+  await click(classNames.container)
+  assert.notOk(find(classNames.input))
 })
 
-test('it should send the close action if disabled', function (assert) {
-  this.set('enabled', true);
+test("it should send the close action if disabled", async function(assert) {
+  this.set("enabled", true)
+
   this.render(hbs`{{ember-inline-edit
                     enabled=enabled
-                    value=value}}`);
+                    value=value}}`)
 
-  this.$('.ember-inline-edit').click();
-  assert.equal(this.$('.ember-inline-edit-input').length, 1)
-  assert.equal(this.$('.ember-inline-edit-save').length, 1)
+  await click(classNames.container)
+  assert.ok(find(classNames.input))
 
-  this.set('enabled', false);
-  assert.equal(this.$('.ember-inline-edit-input').length, 0)
-  assert.equal(this.$('.ember-inline-edit-save').length, 0)
-  assert.equal(this.get('value', 'closed'))
+  this.set("enabled", false)
+
+  assert.notOk(find(classNames.input))
+  assert.equal(this.get("value", "closed"))
 })
 
-test('it should gain the .disabled class if not enabled', function (assert) {
-  this.set('enabled', true);
+test("it should gain the .disabled class if not enabled", function(assert) {
+  this.set("enabled", true)
+
   this.render(hbs`{{ember-inline-edit
                     enabled=enabled
-                    value=value}}`);
+                    value=value}}`)
 
-  assert.equal(this.$('.ember-inline-edit:not(.disabled)').length, 1)
+  assert.ok(find(".ember-inline-edit:not(.disabled)"))
 
-  this.set('enabled', false);
-  assert.equal(this.$('.ember-inline-edit.disabled').length, 1)
+  this.set("enabled", false)
+  assert.ok(find(".ember-inline-edit.disabled"))
 })
 
-test('it should render the editable inside block if a block is present', function(assert) {
+test("it should render the editable inside block if a block is present", function(
+  assert
+) {
   this.render(hbs`{{#ember-inline-edit as |inline-edit|}}
                     {{#inline-edit.editable}}
                       Test
                     {{/inline-edit.editable}}
-                  {{/ember-inline-edit}}`);
+                  {{/ember-inline-edit}}`)
 
-  assert.equal(this.$().text().trim(), 'Test')
+  assert.equal(find(classNames.container).innerText.trim(), "Test")
+})
 
-});
-
-test('it should render the editor inside block if a block is present', function(assert) {
+test("it should render the editor inside block if a block is present", function(
+  assert
+) {
   this.render(hbs`{{#ember-inline-edit as |inline-edit|}}
-                    {{#inline-edit.editor}}
+                    {{#inline-edit.editor class="editor"}}
                       Test
                     {{/inline-edit.editor}}
-                  {{/ember-inline-edit}}`);
+                  {{/ember-inline-edit}}`)
 
-  const text = this.$().text().trim()
+  const text = find(".editor").innerText.trim()
+
   assert.ok(/Test/.test(text), "renders the custom component")
   assert.ok(/Save/.test(text), "renders the save button")
-});
+})
 
-test('it should toggle the custom editor when clicked on the editable', function(assert) {
+test("it should toggle the custom editor when clicked on the editable", async function(
+  assert
+) {
   this.render(hbs`{{#ember-inline-edit as |inline-edit|}}
-                    {{#inline-edit.editable}}
-                      <div class="editable">
-                        Editable
-                      </div>
+                    {{#inline-edit.editable class="editable"}}
+                      Editable
                     {{/inline-edit.editable}}
 
-                    {{#inline-edit.editor}}
-                      <div class="editor">
-                        Editor
-                      </div>
+                    {{#inline-edit.editor class="editor"}}
+                      Editor
                     {{/inline-edit.editor}}
-                  {{/ember-inline-edit}}`);
+                  {{/ember-inline-edit}}`)
 
-  assert.ok(this.$('.editor').parent().hasClass("is-hidden"), "editor is hidden by default")
-  assert.ok(this.$('.editable').parent().hasClass("is-visible"), "editable is visible by default")
+  assert.ok(
+    find(".editor").classList.contains("is-hidden"),
+    "editor is hidden by default"
+  )
+  assert.ok(
+    find(".editable").classList.contains("is-visible"),
+    "editable is visible by default"
+  )
 
-  $('.editor').trigger('click')
+  await click(".editor")
 
-  assert.ok(this.$('.editor').parent().hasClass("is-visible"), "editor is visible after click")
-  assert.ok(this.$('.editable').parent().hasClass("is-hidden"), "editable is hidden after click")
-});
+  assert.ok(
+    find(".editor").classList.contains("is-visible"),
+    "editor is visible after click"
+  )
+  assert.ok(
+    find(".editable").classList.contains("is-hidden"),
+    "editable is hidden after click"
+  )
+})
