4aae98571830144053e138c5b7a4cf7ee9a5f674
johanrd <johanroed@gmail.com>
2021-08-18 09:06:06 +0200

    Deprecate the Ember Global
    See https://deprecations.emberjs.com/v3.x/#toc_ember-global

--- addon/helpers/in-arr.js
+++ addon/helpers/in-arr.js
@@ -1,15 +1,17 @@
-import Ember from 'ember';
+import { helper } from '@ember/component/helper';
+import { isEmpty } from '@ember/utils';
+import { warn } from '@ember/debug';
 
 export function inArr(params/*, hash*/) {
   const haystack = params[0]
   const needle = params[1]
 
-  if (Ember.isEmpty(haystack) || Ember.isEmpty(needle)) {
-    Ember.warn('in-arr params cannot be empty')
+  if (isEmpty(haystack) || isEmpty(needle)) {
+    warn('in-arr params cannot be empty', 'ember-debug.empty-in-arr-params')
     return
   }
   
   return haystack.indexOf(needle) > -1;
 }
 
-export default Ember.Helper.helper(inArr);
+export default helper(inArr);
